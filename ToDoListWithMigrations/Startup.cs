5108f60ed82e0d7a05a43ef6102377b2da4f490d
patrick napper <patrick@patricknapper.com>
2018-01-04 10:48:53 -0800

    add home controller and index view, fix startup file.

--- ToDoListWithMigrations/Startup.cs
+++ ToDoListWithMigrations/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using ToDoListWithMigrations.Models;
+using Microsoft.Extensions.Logging;
 
 namespace ToDoListWithMigrations
 {
@@ -28,19 +29,28 @@ namespace ToDoListWithMigrations
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(); 
             services.AddEntityFramework()
                     .AddDbContext<ToDoDbContext>(options =>
                         options.UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
-        {
-            
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+			loggerFactory.AddConsole();
+
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
+			});
+		}
     }
 }
