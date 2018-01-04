bcb38162e057ddffabc8c3401e771987ce2cf72e
patrick napper <patrick@patricknapper.com>
2018-01-04 08:52:07 -0800

    create initial ToDoList app using migrations to generate database.

---
+++ ToDoListWithMigrations/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using ToDoListWithMigrations.Models;
+
+namespace ToDoListWithMigrations
+{
+    public class Startup
+    {
+		public IConfigurationRoot Configuration { get; set; }
+		public Startup(IHostingEnvironment env)
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(env.ContentRootPath)
+				.AddJsonFile("appsettings.json");
+			Configuration = builder.Build();
+		}
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddEntityFramework()
+                    .AddDbContext<ToDoDbContext>(options =>
+                        options.UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
